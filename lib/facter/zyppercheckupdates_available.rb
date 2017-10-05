070b281f10b8343b735b646f7c866f50221a8cf7
Marcel Ebbrecht <marcel.ebbrecht@googlemail.com>
2017-10-05 10:29:06 +0200

    Update

--- lib/facter/zyppercheckupdates_available.rb
+++ lib/facter/zyppercheckupdates_available.rb
@@ -1,36 +1,48 @@
-Facter.add(:updates_available_list) do
-    setcode do
-	    Facter::Core::Execution.exec('cat /tmp/zypperupdateslist')
-    end
+Facter.add("zypper_updates_available_list") do
+	setcode do
+		if File.exist? '/tmp/zypperupdateslist'
+			Facter::Core::Execution.exec('cat /tmp/zypperupdateslist')
+		end
+	end
 end
 
-Facter.add(:updates_available) do
-    setcode do
-	    Facter::Core::Execution.exec('cat /tmp/zypperupdates')
-    end
+Facter.add("zypper_updates_available") do
+	setcode do
+		if File.exist? '/tmp/zypperupdates'
+			Facter::Core::Execution.exec('cat /tmp/zypperupdates')
+		end
+	end
 end
 
-Facter.add(:patches_available_list) do
-    setcode do
-	    Facter::Core::Execution.exec('cat /tmp/zypperpatcheslist')
-    end
+Facter.add("zypper_patches_available_list") do
+	setcode do
+		if File.exist? '/tmp/zypperpatcheslist'
+			Facter::Core::Execution.exec('cat /tmp/zypperpatcheslist')
+		end
+	end
 end
 
-Facter.add(:patches_available) do
-    setcode do
-	    Facter::Core::Execution.exec('cat /tmp/zypperpatches')
-    end
+Facter.add("zypper_patches_available") do
+	setcode do
+		if File.exist? '/tmp/zypperpatches'
+			Facter::Core::Execution.exec('cat /tmp/zypperpatches')
+		end
+	end
 end
 
-Facter.add(:patches_available_security_list) do
-    setcode do
-	    Facter::Core::Execution.exec('cat /tmp/zypperpatcheslistsecurity')
-    end
+Facter.add("zypper_patches_available_security_list") do
+	setcode do
+		if File.exist? '/tmp/zypperpatchessecuritylist'
+			Facter::Core::Execution.exec('cat /tmp/zypperpatchessecuritylist')
+		end
+	end
 end
 
-Facter.add(:patches_available_security) do
-    setcode do
-	    Facter::Core::Execution.exec('cat /tmp/zypperpatchessecurity')
-    end
+Facter.add("zypper_patches_available_security") do
+	setcode do
+		if File.exist? '/tmp/zypperpatchessecurity'
+			Facter::Core::Execution.exec('cat /tmp/zypperpatchessecurity')
+		end
+	end
 end
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -43,27 +43,49 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class zyppercheckupdates {
-    exec {
-        "zypper-updates":
-            path => '/sbin:/usr/sbin:/usr/local/sbin:/root/bin:/usr/local/bin:/usr/bin:/bin:/usr/games',
-            command => "if [ $(zypper -qn lu | grep $(arch) | cut -d "|" -f 3 | sed 's/ //g' | wc -l) -eq 0 ]; then printf no > /tmp/zypperupdates; printf none > /tmp/zypperupdateslist; else printf yes > /tmp/zypperupdates; zypper -qn lu | grep $(arch) | cut -d "|" -f 3 | sed 's/ //g' | tr '\\n' ' ' > /tmp/zypperupdateslist; fi; exit 0",
-            timeout => 30,
-            notify => undef,
-    }
+        file {
+                "zypper_check_updates":
+                        ensure => 'file',
+                        content => "#!/bin/bash
+PATH='/root/perl5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin:/root/bin'
+LANG=C
 
-    exec {
-        "zypper-patches":
-            path => '/sbin:/usr/sbin:/usr/local/sbin:/root/bin:/usr/local/bin:/usr/bin:/bin:/usr/games',
-            command => "if [ $(zypper -qn lp | grep -v security | grep -v '+--'| grep '|' | grep -v 'tatus' | cut -d '|' -f 2 | sed 's/ //g' | wc -l) -eq 0 ]; then printf no > /tmp/zypperpatches; printf none > /tmp/zypperpatcheslist; else printf yes > /tmp/zypperpatches; zypper -qn lp | grep -v security | grep -v '+--'| grep -v 'tatus' | grep '|' | cut -d '|' -f 2 | sed 's/ //g' | tr '\\n' ' ' > /tmp/zypperpatcheslist; fi; exit 0",
-            timeout => 30,
-            notify => undef,
-    }
+/usr/bin/printf ''
+if [ \$(zypper -qn lu | grep \$(arch) | cut -d '|' -f 3 | sed 's/ //g' | wc -l) -eq 0 ]; then
+	printf no > /tmp/zypperupdates
+	printf none > /tmp/zypperupdateslist
+else
+	printf yes > /tmp/zypperupdates
+	zypper -qn lu | grep $(arch) | cut -d '|' -f 3 | sed 's/ //g' | tr '\\n' ' ' > /tmp/zypperupdateslist
+fi
 
-    exec {
-        "zypper-patches-security":
-            path => '/sbin:/usr/sbin:/usr/local/sbin:/root/bin:/usr/local/bin:/usr/bin:/bin:/usr/games',
-            command => "if [ $(zypper -qn lp | grep security | grep -v '+--'| grep '|' | grep -v 'tatus' | cut -d '|' -f 2 | sed 's/ //g' | wc -l) -eq 0 ]; then printf no > /tmp/zypperpatchessecurity; printf none > /tmp/zypperpatchessecuritylist; else printf yes > /tmp/zypperpatchessecurity; zypper -qn lp | grep security | grep -v '+--'| grep -v 'tatus' | grep '|' | cut -d '|' -f 2 | sed 's/ //g' | tr '\\n' ' ' > /tmp/zypperpatchessecuritylist; fi; exit 0",
-            timeout => 30,
-            notify => undef,
-    }
+if [ \$(zypper -qn lp | grep -v security | grep -v '+--'| grep '|' | grep -v 'tatus' | cut -d '|' -f 2 | sed 's/ //g' | wc -l) -eq 0 ]; then
+	printf no > /tmp/zypperpatches
+	printf none > /tmp/zypperpatcheslist
+else
+	printf yes > /tmp/zypperpatches
+	zypper -qn lp | grep -v security | grep -v '+--'| grep -v 'tatus' | grep '|' | cut -d '|' -f 2 | sed 's/ //g' | tr '\\n' ' ' > /tmp/zypperpatcheslist
+fi
+
+if [ \$(zypper -qn lp | grep security | grep -v '+--'| grep '|' | grep -v 'tatus' | cut -d '|' -f 2 | sed 's/ //g' | wc -l) -eq 0 ]; then
+	printf no > /tmp/zypperpatchessecurity
+	printf none > /tmp/zypperpatchessecuritylist
+else
+	printf yes > /tmp/zypperpatchessecurity
+	zypper -qn lp | grep security | grep -v '+--'| grep -v 'tatus' | grep '|' | cut -d '|' -f 2 | sed 's/ //g' | tr '\\n' ' ' > /tmp/zypperpatchessecuritylist
+fi
+
+exit 0
+",
+                        path => '/usr/local/bin/zypper_check_updates.sh',
+                        owner => 'root',
+                        group => 'root',
+                        mode => '0755',
+        }
+
+	exec {
+		"zypper-updates":
+			command => '/usr/local/bin/zypper_check_updates.sh',
+			timeout => 60,
+	}
 }
